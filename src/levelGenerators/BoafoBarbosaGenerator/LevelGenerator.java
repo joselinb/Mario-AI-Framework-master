38bc4cdd812762e45501a9616054bcb03f8b398f
joselinb <joselinb2001@gmail.com>
2023-11-10 14:57:53 -0500

    code for level generator and slice

--- src/levelGenerators/BoafoBarbosaGenerator/LevelGenerator.java
+++ src/levelGenerators/BoafoBarbosaGenerator/LevelGenerator.java
@@ -1,4 +1,176 @@
 package levelGenerators.BoafoBarbosaGenerator;
 
-public class LevelGenerator {
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import engine.core.MarioLevelGenerator;
+import engine.core.MarioLevelModel;
+import engine.core.MarioTimer;
+
+
+public class LevelGenerator implements MarioLevelGenerator {
+    private final String LEVEL_FOLDER = "levels/ore/";
+    // list of all slices this level generator knows of
+    private List<Slice> slices;
+    // indexes of all slices with a Mario start tile
+    private List<Integer> starts;
+    // list of slices with a flag tile
+    private List<Integer> ends;
+
+    /**
+     * Basic constructor, reads all level files in LEVEL_FOLDER
+     */
+    public LevelGenerator() {
+        slices = new ArrayList<Slice>();
+        starts = new ArrayList<Integer>();
+        ends = new ArrayList<Integer>();
+        // Get all filenames in the lePath directory}
+        for (final File fileEntry : new File(LEVEL_FOLDER).listFiles()) {
+            if (!fileEntry.isDirectory()) {
+                parseIn(fileEntry.getAbsolutePath());
+            }
+        }
+    }
+
+    /**
+     * Reads in a level file from the filesystem, splitting it into 1-column
+     * wide slices and adding them to a Markov transition table
+     * @param filename the path to the file to read in
+     */
+    public void parseIn(String filename) {
+        System.out.println("Reading " + filename);
+        FileReader fr;
+        List<char[]> cols = new ArrayList<char[]>();
+        try {
+            fr = new FileReader(filename);
+            int i; // the character read by fr
+            int column = 0; // the column number being read
+            int row = 0; // the row number being read
+            boolean firstLine = true;
+            // read through the file and separate out each column into arrays of characters
+            while ((i = fr.read()) != -1) {
+                if ((char) i == '\n') {
+                    if (firstLine) {
+                        firstLine = false;
+                    }
+                    row++;
+                    column = 0;
+                } else {
+                    if (firstLine) {
+                        cols.add(new char[16]);
+                    }
+                    cols.get(column)[row] = (char) i;
+                    column++;
+                }
+            }
+            fr.close();
+
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            System.err.println("Error with file " + filename);
+            return;
+        }
+
+        // convert char arrays into slices
+        Slice lastSlice = null;
+        for (char[] c : cols) {
+            // see if we've seen this exact sequence of characters before
+            Slice tempSlice = null;
+            for (Slice s : slices) {
+                if (s.toString().equals(String.valueOf(c))) {
+                    tempSlice = s;
+                    break;
+                }
+            }
+            if (tempSlice == null) {
+                // this is a new slice
+                tempSlice = new Slice();
+                for (int i = 0; i < 16; ++i) {
+                    if ((char) c[i] == 'M') {
+                        tempSlice.setMario(true);
+                        starts.add(slices.size());
+                    }
+                    if ((char) c[i] == 'F') {
+                        tempSlice.setFlag(true);
+                        ends.add(slices.size());
+                    }
+                    tempSlice.addInChar(c[i], i);
+                }
+                slices.add(tempSlice);
+            }
+            // add this as a followup to the last slice we saw
+            if (lastSlice != null) {
+                lastSlice.addFollow(tempSlice);
+            }
+            lastSlice = tempSlice;
+        }
+    }
+
+    /**
+     * Generates a level
+     * @param model a MarioLevelModel object
+     * @parm timer a MarioTimer object
+     * @return the string version of a Markov-generated level
+     */
+    @Override
+    public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
+        // initialize new random number generator
+        Random rng = new Random();
+
+        // start with a clear model
+        model.clearMap();
+
+        // pick a starting slice that has a Mario start block, or just a random
+        // one if no slices have a Mario start
+        Slice curSlice;
+        if (starts.size() > 0) {
+            curSlice = slices.get(starts.get(rng.nextInt(starts.size())));
+        } else {
+            curSlice = slices.get(rng.nextInt(slices.size()));
+        }
+        for (int i = 0; i < 16; ++i) {
+            model.setBlock(0, i, curSlice.getPiece(i));
+        }
+
+        int x = 1;
+        boolean alreadyFlag = false;
+        int prevHeight = curSlice.getGroundHeight();
+        while (x < model.getWidth() - 1) {
+            do {
+                curSlice = curSlice.getNext(rng);
+                // make sure that the next slice has followups and isn't too high to jump to from here
+            } while (curSlice.getTotalFollow() < 1 && curSlice.getGroundHeight() < prevHeight + 4);
+            for (int i = 0; i < 16; ++i) {
+                model.setBlock(x,  i, curSlice.getPiece(i));
+            }
+            if (curSlice.getFlag()) {
+                alreadyFlag = true;
+                break;
+            }
+            x++;
+        }
+
+        // if we haven't already added a flag, put a flag slice at the end
+        if (!alreadyFlag) {
+            // pick a random flag slice and add it to the end
+            curSlice = slices.get(ends.get(rng.nextInt(ends.size())));
+            for (int i = 0; i < 16; ++i) {
+                model.setBlock(model.getWidth() - 1, i, curSlice.getPiece(i));
+            }
+        }
+
+        return model.getMap();
+    }
+
+    /**
+     * gets the name of this level generator
+     * @return the name of this generator
+     */
+    @Override
+    public String getGeneratorName() {
+        return "MarkovChainGenerator";
+    }
 }
--- src/levelGenerators/BoafoBarbosaGenerator/Slice.java
+++ src/levelGenerators/BoafoBarbosaGenerator/Slice.java
@@ -1,4 +1,153 @@
 package levelGenerators.BoafoBarbosaGenerator;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Slice {
+    // total number of followups
+    private int totalFollow;
+    // the blocks in this slice. pieces[0] is the top of the screen, pieces[15] is the bottom
+    private char[] pieces;
+    // a map that keeps track of each slice that can follow this one and the number of times that it did
+    private Map<Slice, Integer> followTimes;
+    // whether this slice contains a flag character
+    private boolean isFlag;
+    // whether this slice contains a Mario start character
+    private boolean isMario;
+
+    /**
+     * Basic constructor
+     */
+    public Slice(){
+        pieces = new char[16];
+        totalFollow = 0;
+        followTimes = new HashMap<Slice, Integer>();
+    }
+
+    /**
+     * @return a string representing the blocks in this slice
+     */
+    public String toString() {
+        return String.valueOf(pieces);
+    }
+
+    /**
+     * Replaces the character at the index slice with the given character
+     * @param a the character to add
+     * @param space the index to replace with the input character
+     */
+    public void addInChar(char a, int space){
+        pieces[space] = a;
+    }
+
+    /**
+     * Specify whether this slice contains a flag ('F')
+     * @param isFlag the new isFlag value
+     */
+    public void setFlag(boolean isFlag) {
+        this.isFlag = isFlag;
+    }
+
+    /**
+     * Returns whether this slice contains a flag ('F')
+     * @return the value of isFlag
+     */
+    public boolean getFlag() {
+        return this.isFlag;
+    }
+
+    /**
+     * Specify whether this slice contains a Mario start location ('M')
+     * @param isMario the new isMario value
+     */
+    public void setMario(boolean isMario) {
+        this.isMario = isMario;
+    }
+
+    /**
+     * Returns whether this slice contains a Mario start ('M')
+     * @return the value of isMario
+     */
+    public boolean getMario() {
+        return this.isMario;
+    }
+
+    /**
+     * Get the character at the specified index in this slice
+     * @param num the index of the piece you want
+     * @return the char in that index
+     */
+    public char getPiece(int num){
+        return(pieces[num]);
+    }
+
+    /**
+     * totalFollow getter
+     * @return the total number of followups to this slice
+     */
+    public int getTotalFollow() {
+        return totalFollow;
+    }
+
+    /**
+     * Add a followup slice to this one
+     * @param slice the slice that follows this one
+     */
+    public void addFollow(Slice slice){
+        if(followTimes.containsKey(slice)){
+            followTimes.put(slice, followTimes.get(slice) + 1);
+        }
+        else{
+            followTimes.put(slice, 1);
+        }
+        totalFollow++;
+    }
+
+    /**
+     * Picks a random slice from its followups based on Markov Chain rules
+     * @param rng a random number generator
+     * @return the chosen slice
+     */
+    public Slice getNext(Random rng){
+        int nextRand = rng.nextInt(totalFollow);
+        int tempTotal = 0;
+        for(Map.Entry<Slice, Integer> b : followTimes.entrySet()){
+            tempTotal += b.getValue();
+            if(nextRand < tempTotal){
+                return(b.getKey());
+            }
+        }
+        return(null);
+    }
+
+    /**
+     * Checks whether a given block is solid
+     * @param c the character to check
+     * @return true if the character represents a solid block, false otherwise
+     */
+    private boolean isSolid(char c) {
+        return c == 'X' || c == '#' || c == '@' || c == '!' || c == 'B' || c == 'C' ||
+                c == 'Q' || c == '<' || c == '>' || c == '[' || c == ']' || c == '?' ||
+                c == 'S' || c == 'U' || c == 'D' || c == '%' || c == 't' || c == 'T';
+    }
+
+    /**
+     * Finds the height of the lowest solid ground Mario can stand on
+     * @return the height of the ground, starting from 0
+     */
+    public int getGroundHeight() {
+        int out = -1;
+        for (int i = 15; i >= 0; --i) {
+            if (isSolid(pieces[i])) {
+                out = i;
+            } else {
+                if (out > -1) {
+                    return out;
+                }
+            }
+        }
+        // if we get here if means we never found a solid block with air above it
+        return -1;
+    }
 }
